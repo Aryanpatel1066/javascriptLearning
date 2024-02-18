9a7e79401b12c45ce9fb3ba1963d3b4e96c244a0
aryanPatel <aryanpatel1248@gmail.com>
2024-02-18 21:53:03 +0530

    third commit of the day some solve the question on reast and spread operator

--- day7.js
+++ day7.js
@@ -44,3 +44,64 @@ const address = {
 // console.log(person,address)
 const fullProfile = {...person,...address};
 console.log(fullProfile)
+
+//4 Write a function that takes any number of arguments using the rest operator and calculates their sum.
+
+const sumOfElement = (...numbers)=>{
+    let sum = numbers.reduce((total,numbers)=>total+numbers,0);
+    return sum
+}
+console.log(sumOfElement(10,20,-9))
+
+//5 Create a function that accepts an array of numbers and uses the rest operator to calculate their sum.
+
+const myArr = [10,20,30];
+const sumOfArray = ([...numbers])=>{
+    let sum = numbers.reduce((total,numbers)=>total+numbers,0);
+    return sum;
+};
+console.log(sumOfArray(myArr));
+
+//6Write a function that takes any number of arguments and returns the maximum value using the rest operator.
+
+const findMax = (...numbers)=>{
+    let max = numbers.reduce((max,numbers)=>max > numbers);
+    return max;
+};
+ 
+console.log(findMax(10,20,30))
+
+//7 Write a function that clones an array using the spread operator.
+
+const oldArr = [10,20,44];
+const newArr = [...oldArr,88];
+console.log(newArr)
+
+// solve some question on spread operator
+
+//8 Write a function that takes two arrays and uses the spread operator to concatenate them.
+
+const twoArr = (arr1,arr2)=>{
+  const newArr = [...arr1,...arr2];
+  return newArr;
+}
+const a = [10,30];
+const b = [50,50]
+console.log(twoArr(a,b))
+
+//9 Create a function that merges two objects using the spread operator.
+
+const obj1 = {
+    name:"aary",
+    age:17
+}
+const obj2 = {
+    name:'ldrp itr',
+    city:"green city gandhinagar"
+}
+const takeTwoObj = (obj1,obj2)=>{
+    const newObj = {...obj1,...obj2};
+    return newObj;
+}
+const rs = takeTwoObj(obj1,obj2);
+console.log(rs);
